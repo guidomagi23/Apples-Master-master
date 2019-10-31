a4e95ba68b6dfe2078745d45e328d4f306ddf073
Adriel <adriel096@gmail.com>
2019-10-31 16:14:40 -0300

    corregido boludeces: botones correo, boton segunda actividad.

--- app/src/main/java/com/example/appies/ActividadSeleccionarMateriaPrimero.java
+++ app/src/main/java/com/example/appies/ActividadSeleccionarMateriaPrimero.java
@@ -3,8 +3,10 @@ package com.example.appies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
@@ -40,5 +42,25 @@ public class ActividadSeleccionarMateriaPrimero extends AppCompatActivity {
         startActivity(ing);
     }
 
+
+    public void accionarBotonEnviar(View view){
+        String[] TO = {"noties@gmail.com"}; //aquí pon tu correo
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        // Esto podrás modificarlo si quieres, el asunto y el cuerpo del mensaje
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Material para Noties");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "...");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+            finish();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(ActividadSeleccionarMateriaPrimero.this, "No tienes clientes de email instalados.", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
--- app/src/main/java/com/example/appies/ActividadSeleccionarMateriaSegundo.java
+++ app/src/main/java/com/example/appies/ActividadSeleccionarMateriaSegundo.java
@@ -1,8 +1,10 @@
 package com.example.appies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,26 @@ public class ActividadSeleccionarMateriaSegundo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_seleccionar_materia_segundo);
+    }
+
+    public void accionarBotonEnviar(View view){
+        String[] TO = {"noties@gmail.com"}; //aquí pon tu correo
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        // Esto podrás modificarlo si quieres, el asunto y el cuerpo del mensaje
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Material para Noties");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "...");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+            finish();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(ActividadSeleccionarMateriaSegundo.this, "No tienes clientes de email instalados.", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
